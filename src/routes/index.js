233d2f5dc20868be98626728a6cbb3ee9dcd8999
frederico <fredericonakajimanakajima@gmail.comm>
2024-06-23 09:15:18 -0300

    post,update

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+const { Router} = require("express");
+
+const usersRoutes = require("./users.routes");
+
+
+
+
+
+const routes = new Router()
+
+routes.use("/users", usersRoutes)
+
+
+module.exports = routes
