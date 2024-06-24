616f0994ccbec997785336e93ae3175174e3a909
frederico <fredericonakajimanakajima@gmail.comm>
2024-06-24 17:01:47 -0300

    tags, conclusão backend

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const { Router} = require("express");
 
 const usersRouter = require("./users.routes");
 const notesRouter = require("./notes.routes");
-
+const tagsRouter = require("./tags.routes");
 
 
 
@@ -10,5 +10,8 @@ const routes = Router()
 
 routes.use("/users",usersRouter)
 routes.use("/notes",notesRouter)
+routes.use("/tags",tagsRouter)
+
+
 
 module.exports = routes
